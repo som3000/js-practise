3f564ddd71c062e1b5705a4bb9ae28538a399742
som3000 <guptanathsom@gmail.com>
2025-09-19 17:30:02 +0530

    Print an ap with inputs ( first term , no of term , difference )

---
+++ print_ap.js
@@ -0,0 +1,15 @@
+const firstTerm = 4
+const noTerm = 10
+const diff = 3
+let currentTermNum = 0
+let currentTerm = firstTerm
+let apNow = firstTerm
+let sum = 0
+console.log(currentTerm);
+while (currentTermNum < (noTerm-1)) {
+    currentTerm = currentTerm + diff
+    currentTermNum = currentTermNum + 1
+    console.log(currentTerm);
+
+    
+}
