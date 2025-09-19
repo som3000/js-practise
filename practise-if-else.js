9e7aaf6645afc3e2a2c24a17a33c52a78894b544
som3000 <guptanathsom@gmail.com>
2025-09-19 12:41:49 +0530

    Practised how does a if else operation works.

---
+++ practise-if-else.js
@@ -0,0 +1,11 @@
+let x = 100
+let paise = 52
+
+if (paise > 50){
+    console.log("total amount =", x+1);
+   
+} else {
+    console.log("total amount =", x);
+    
+}
+console.log('Round off done. Thanks for your time.');
