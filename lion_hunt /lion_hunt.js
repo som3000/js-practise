a876f209dfd46a1f1192c0a33b9d9c48e881690b
som3000 <guptanathsom@gmail.com>
2025-09-20 14:14:13 +0530

    Update now can tell the distance between lion and zebra , ordered.
    Now have to make it less complex as  if inside while inside if inside while. 
    Now i get the sense of logic in how to proceed.

--- lion_hunt /lion_hunt.js
+++ lion_hunt /lion_hunt.js
@@ -1,8 +1,28 @@
-const savana = "L   Z"
-let leastSpaces = 0
+const savana = "L         Z"
 const zebra = 'Z'
 const lion = 'L'
 let currentPlace = 0
+let checkingPlace = currentPlace
 const lengthOfSavana = savana.length
+let distanceBetweenThem = 0 
+let zebraFound = false
 
-console.log(lengthOfSavana);
+while (currentPlace <= lengthOfSavana) {
+   
+    if (savana[currentPlace] === lion) {
+        checkingPlace ++
+        zebraFound = false
+       
+        while (!zebraFound) {
+            if (savana[checkingPlace] === zebra){
+                distanceBetweenThem = checkingPlace - currentPlace
+                break
+            } else {
+                checkingPlace ++
+
+            }
+        }
+    }
+    currentPlace ++
+} 
+console.log('spaces between Lion and Zebra =' , distanceBetweenThem - 1);
